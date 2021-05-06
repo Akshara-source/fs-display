859bd8b01e16f0741b6cf041534d81f8bb848f5e
akshara <aksharas314@gmail.com>
2021-05-06 11:31:59 +0530

    dynamic html page

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ const port = process.env.PORT || 3000;
 
 
 http.createServer(function (req, res) {
-    fs.readFile('notes.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
+    fs.readFile('notes.json',function(err,data){
+        const heading = "My Profile";
+
+        const datastring = data.toString();
+        const dataObj = JSON.parse(datastring)
+
+        const dynamicContent = '<!DOCTYPE html>'
+        + '<html><head></head><body><h1>'+heading+'</h1><p>Name:'+dataObj.name+'</p><p>Age:'+dataObj.age+'</p><p>company:'+dataObj.company+'</p></body></html>';
+
+        res.write(dynamicContent);
+
         return res.end();
-      });
+
+    })
     
 }).listen(port);
+
+
 
 
 
